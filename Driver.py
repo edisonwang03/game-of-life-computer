3a6ec00b355becd711f2130054b2efed6821ef2d
Edison Wang <edisonwang03@gmail.com>
2022-11-13 20:42:06 -0500

    Added an AND gate

--- Driver.py
+++ Driver.py
@@ -10,14 +10,14 @@ GRAY = (50,50,50)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
-LIGHT_RED = (155,0,0)
+LIGHT_RED = (255,100,100)
 
 
 # Grid constants & initializing the grid
-ROWS = 100
-COLS = 100
-TILE_WIDTH = 6
-TILE_HEIGHT = 6
+ROWS = 80
+COLS = 150
+TILE_WIDTH = 4
+TILE_HEIGHT = 4
 MARGIN_SIZE = 1
 gol = GameOfLife.gameoflifegrid(ROWS,COLS)
 
@@ -45,19 +45,47 @@ CLEAR_GRID_Y = 80
 CLEAR_GRID_WIDTH = 220
 CLEAR_GRID_HEIGHT = 20
 
-# NOT gate without input button
-NOT_OFF_TEXT = BUTTON_FONT.render("Create a NOT Gate without input", True, BLACK)
-NOT_OFF_X = 840
-NOT_OFF_Y = 130
-NOT_OFF_WIDTH = 220
-NOT_OFF_HEIGHT = 20
+# NOT gate button (0)
+NOT_0_TEXT = BUTTON_FONT.render("Create a NOT Gate (0)", True, BLACK)
+NOT_0_X = 840
+NOT_0_Y = 130
+NOT_0_WIDTH = 220
+NOT_0_HEIGHT = 20
 
-# NOT gate with input button
-NOT_ON_TEXT = BUTTON_FONT.render("Create a NOT Gate with input", True, BLACK)
-NOT_ON_X = 840
-NOT_ON_Y = 160
-NOT_ON_WIDTH = 220
-NOT_ON_HEIGHT = 20
+# NOT gate button (1)
+NOT_1_TEXT = BUTTON_FONT.render("Create a NOT Gate with (1)", True, BLACK)
+NOT_1_X = 840
+NOT_1_Y = 160
+NOT_1_WIDTH = 220
+NOT_1_HEIGHT = 20
+
+# AND gate button (00)
+AND_00_TEXT = BUTTON_FONT.render("Create an AND Gate with (00)", True, BLACK)
+AND_00_X = 840
+AND_00_Y = 210
+AND_00_WIDTH = 220
+AND_00_HEIGHT = 20
+
+# AND gate button (01)
+AND_01_TEXT = BUTTON_FONT.render("Create an AND Gate with (01)", True, BLACK)
+AND_01_X = 840
+AND_01_Y = 240
+AND_01_WIDTH = 220
+AND_01_HEIGHT = 20
+
+# AND gate button (10)
+AND_10_TEXT = BUTTON_FONT.render("Create an AND Gate with (10)", True, BLACK)
+AND_10_X = 840
+AND_10_Y = 270
+AND_10_WIDTH = 220
+AND_10_HEIGHT = 20
+
+# AND gate button (11)
+AND_11_TEXT = BUTTON_FONT.render("Create an AND Gate with (11)", True, BLACK)
+AND_11_X = 840
+AND_11_Y = 300
+AND_11_WIDTH = 220
+AND_11_HEIGHT = 20
 
 
 
@@ -84,11 +112,20 @@ while not done:
             if x >= CLEAR_GRID_X and x <= CLEAR_GRID_X + CLEAR_GRID_WIDTH and y >= CLEAR_GRID_Y and y <= CLEAR_GRID_Y + CLEAR_GRID_HEIGHT:
                 gol.clearGrid()
             
-            if x >= NOT_OFF_X and x <= NOT_OFF_X + NOT_OFF_WIDTH and y >= NOT_OFF_Y and y <= NOT_OFF_Y + NOT_OFF_HEIGHT:
-                gol.makeNOTGateWithoutInput()
-                
-            if x >= NOT_ON_X and x <= NOT_ON_X + NOT_ON_WIDTH and y >= NOT_ON_Y and y <= NOT_ON_Y + NOT_ON_HEIGHT:
-                gol.makeNOTGateWithInput()
+            if x >= NOT_0_X and x <= NOT_0_X + NOT_0_WIDTH and y >= NOT_0_Y and y <= NOT_0_Y + NOT_0_HEIGHT:
+                gol.spawnNOTGate(False)
+            if x >= NOT_1_X and x <= NOT_1_X + NOT_1_WIDTH and y >= NOT_1_Y and y <= NOT_1_Y + NOT_1_HEIGHT:
+                gol.spawnNOTGate(True)
+            
+            if x >= AND_00_X and x <= AND_00_X + AND_00_WIDTH and y >= AND_00_Y and y <= AND_00_Y + AND_00_HEIGHT:
+                gol.spawnANDGate(False,False)     
+            if x >= AND_01_X and x <= AND_01_X + AND_01_WIDTH and y >= AND_01_Y and y <= AND_01_Y + AND_01_HEIGHT:
+                gol.spawnANDGate(True,False)
+            if x >= AND_10_X and x <= AND_10_X + AND_10_WIDTH and y >= AND_10_Y and y <= AND_10_Y + AND_10_HEIGHT:
+                gol.spawnANDGate(False,True)
+            if x >= AND_11_X and x <= AND_11_X + AND_11_WIDTH and y >= AND_11_Y and y <= AND_11_Y + AND_11_HEIGHT:
+                gol.spawnANDGate(True,True)
+
       
             # Set that location to one
             if column in range(COLS) and row in range(ROWS):
@@ -128,23 +165,49 @@ while not done:
     # Draw UI
     screen.blit(TITLE_TEXT,(TITLE_X,TITLE_Y))
     
-    if x >= NOT_OFF_X and x <= NOT_OFF_X + NOT_OFF_WIDTH and y >= NOT_OFF_Y and y <= NOT_OFF_Y + NOT_OFF_HEIGHT:
-        pygame.draw.rect(screen,LIGHT_RED,(NOT_OFF_X,NOT_OFF_Y,NOT_OFF_WIDTH,NOT_OFF_HEIGHT))
-    else:
-        pygame.draw.rect(screen,RED,(NOT_OFF_X,NOT_OFF_Y,NOT_OFF_WIDTH,NOT_OFF_HEIGHT))
-    screen.blit(NOT_OFF_TEXT,(NOT_OFF_X,NOT_OFF_Y))
-    
-    if x >= NOT_ON_X and x <= NOT_ON_X + NOT_ON_WIDTH and y >= NOT_ON_Y and y <= NOT_ON_Y + NOT_ON_HEIGHT:
-        pygame.draw.rect(screen,LIGHT_RED,(NOT_ON_X,NOT_ON_Y,NOT_ON_WIDTH,NOT_ON_HEIGHT))
-    else:
-        pygame.draw.rect(screen,RED,(NOT_ON_X,NOT_ON_Y,NOT_ON_WIDTH,NOT_ON_HEIGHT))
-    screen.blit(NOT_ON_TEXT,(NOT_ON_X,NOT_ON_Y))
-    
     if x >= CLEAR_GRID_X and x <= CLEAR_GRID_X + CLEAR_GRID_WIDTH and y >= CLEAR_GRID_Y and y <= CLEAR_GRID_Y + CLEAR_GRID_HEIGHT:
         pygame.draw.rect(screen,LIGHT_RED,(CLEAR_GRID_X,CLEAR_GRID_Y,CLEAR_GRID_WIDTH,CLEAR_GRID_HEIGHT))
     else:
         pygame.draw.rect(screen,RED,(CLEAR_GRID_X,CLEAR_GRID_Y,CLEAR_GRID_WIDTH,CLEAR_GRID_HEIGHT))
     screen.blit(CLEAR_GRID_TEXT,(CLEAR_GRID_X,CLEAR_GRID_Y))
+    
+    
+    if x >= NOT_0_X and x <= NOT_0_X + NOT_0_WIDTH and y >= NOT_0_Y and y <= NOT_0_Y + NOT_0_HEIGHT:
+        pygame.draw.rect(screen,LIGHT_RED,(NOT_0_X,NOT_0_Y,NOT_0_WIDTH,NOT_0_HEIGHT))
+    else:
+        pygame.draw.rect(screen,RED,(NOT_0_X,NOT_0_Y,NOT_0_WIDTH,NOT_0_HEIGHT))
+    screen.blit(NOT_0_TEXT,(NOT_0_X,NOT_0_Y))
+    if x >= NOT_1_X and x <= NOT_1_X + NOT_1_WIDTH and y >= NOT_1_Y and y <= NOT_1_Y + NOT_1_HEIGHT:
+        pygame.draw.rect(screen,LIGHT_RED,(NOT_1_X,NOT_1_Y,NOT_1_WIDTH,NOT_1_HEIGHT))
+    else:
+        pygame.draw.rect(screen,RED,(NOT_1_X,NOT_1_Y,NOT_1_WIDTH,NOT_1_HEIGHT))
+    screen.blit(NOT_1_TEXT,(NOT_1_X,NOT_1_Y))
+    
+    
+    
+    if x >= AND_00_X and x <= AND_00_X + AND_00_WIDTH and y >= AND_00_Y and y <= AND_00_Y + AND_00_HEIGHT:
+        pygame.draw.rect(screen,LIGHT_RED,(AND_00_X,AND_00_Y,AND_00_WIDTH,AND_00_HEIGHT))
+    else:
+        pygame.draw.rect(screen,RED,(AND_00_X,AND_00_Y,AND_00_WIDTH,AND_00_HEIGHT))
+    screen.blit(AND_00_TEXT,(AND_00_X,AND_00_Y))
+    if x >= AND_01_X and x <= AND_01_X + AND_01_WIDTH and y >= AND_01_Y and y <= AND_01_Y + AND_01_HEIGHT:
+        pygame.draw.rect(screen,LIGHT_RED,(AND_01_X,AND_01_Y,AND_01_WIDTH,AND_01_HEIGHT))
+    else:
+        pygame.draw.rect(screen,RED,(AND_01_X,AND_01_Y,AND_01_WIDTH,AND_01_HEIGHT))
+    screen.blit(AND_01_TEXT,(AND_01_X,AND_01_Y))
+    if x >= AND_10_X and x <= AND_10_X + AND_10_WIDTH and y >= AND_10_Y and y <= AND_10_Y + AND_10_HEIGHT:
+        pygame.draw.rect(screen,LIGHT_RED,(AND_10_X,AND_10_Y,AND_10_WIDTH,AND_10_HEIGHT))
+    else:
+        pygame.draw.rect(screen,RED,(AND_10_X,AND_10_Y,AND_10_WIDTH,AND_10_HEIGHT))
+    screen.blit(AND_10_TEXT,(AND_10_X,AND_10_Y))
+    if x >= AND_11_X and x <= AND_11_X + AND_11_WIDTH and y >=AND_11_Y and y <= AND_11_Y + AND_11_HEIGHT:
+        pygame.draw.rect(screen,LIGHT_RED,(AND_11_X,AND_11_Y,AND_11_WIDTH,AND_11_HEIGHT))
+    else:
+        pygame.draw.rect(screen,RED,(AND_11_X,AND_11_Y,AND_11_WIDTH,AND_11_HEIGHT))
+    screen.blit(AND_11_TEXT,(AND_11_X,AND_11_Y))
+    
+    
+
     
     
     # Go ahead and update the screen with what we've drawn.
--- GameOfLife.py
+++ GameOfLife.py
@@ -1,15 +1,11 @@
+import numpy as np
+
 class gameoflifegrid():
     def __init__(self,rows,cols):
         self.rows=rows
         self.cols=cols
-        self.grid = []
-        self.newGrid = []
-        for r in range(self.rows):
-            self.grid.append([])
-            self.newGrid.append([])
-            for c in range(self.cols):
-                self.grid[r].append(0)
-                self.newGrid[r].append(0)
+        self.grid = np.zeros((rows,cols)).reshape((rows,cols))
+        self.newGrid = np.zeros((rows,cols)).reshape((rows,cols))
 
 
     def neighbors(self,r,c):
@@ -46,10 +42,112 @@ class gameoflifegrid():
         self.newGrid = temporaryGrid
         
     def clearGrid(self):
-        for r in range(len(self.grid)):
-            for c in range(len(self.grid[r])):
-                self.grid[r][c] = 0
+        self.grid = np.zeros((self.rows,self.cols)).reshape((self.rows,self.cols))
     
+    def spawnGlider(self,r,c):
+        toSpawn = [[5, 1],[6, 1],[6, 2],[5, 2],[5, 5],[4, 6],[5, 6],[6, 6],[3, 7],[7, 7],[5, 8],[2, 9],[2, 10],
+                  [3, 11], [4, 12],[5, 12],[6, 12],[7, 11],[8, 10],[8, 9],[7, 17],[7, 20],[7, 21],[7, 22],[6, 22],
+                  [5, 21],[6, 26],[5, 26],[1, 26],[0, 26],[1, 28],[5, 28],[2, 29],[3, 29],[4, 29],[4, 30],[3, 30],
+                  [2, 30],[3, 35],[4, 35],[4, 36],[3, 36]]
+        for [x,y] in toSpawn:
+            self.grid[r+x,c+y] = 1
+    
+    def spawnReverseGlider(self,r,c):
+        toSpawn = [[0, 0], [1, 0], [1, 1], [0, 1], [0, 6], [1, 6], [1, 7], [0, 7], [-1, 7], [-1, 6], [-2, 8], 
+                   [-2, 10], [-3, 10],[2, 8], [2, 10], [3, 10], [3, 14], [4, 14], [4, 15], [4, 16], [2, 15], [4, 19], 
+                   [4, 25], [3, 24], [2, 24], [1, 24], [0, 25],[-1, 26], [-1, 27], [5, 26], [5, 27], [2, 28], [2, 30], 
+                   [2, 31], [1, 30], [3, 30], [0, 29], [4, 29], [2, 34], [2, 35], [3, 35], [3, 34]]
+        for [x,y] in toSpawn:
+            self.grid[r+x,c+y] = 1
+    
+    def spawnEaterAnd(self):
+        toSpawn = [[56, 66], [57, 66], [57, 67], [57, 69], [57, 67], [55, 69], [55, 70],[56, 70], [55, 73], [56, 73], 
+                   [56, 74], [55, 74], [59, 66], [59, 67], [60, 67], [60, 66], [57, 68]]
+        for [x,y] in toSpawn:
+            self.grid[x,y] = 1
+            
+    def spawnEaterOr(self):
+        toSpawn = [[72, 82], [73, 82], [73, 83], [73, 84], [73, 85], [72, 86], [71, 86], [71, 85], [71, 89],
+                [72, 89], [72, 90], [71, 90], [75, 82], [75, 83], [76, 83], [76, 82]]
+        for [x,y] in toSpawn:
+            self.grid[x,y] = 1
+
+    def spawnEaterNot(self):
+        toSpawn = [[32, 24], [32, 25], [33, 24], [33, 24], [33, 25], [32, 28], [32, 29], [33, 28],
+                    [34, 29], [34, 30], [34, 31], [34, 32], [33, 32], [36, 31], [36, 32], [37, 32],
+                    [37, 31]]
+        for [x,y] in toSpawn:
+            self.grid[x,y] = 1
+
+    def spawnStopperOr(self):
+        toSpawn = [[58, 111], [59, 111], [58, 112], [60, 112], [60, 113], [60, 114], [61, 114]]
+        for [x,y] in toSpawn:
+            self.grid[x,y] = 1
+
+    def stopperAnd01(self):
+        toSpawn = [[15, 28], [16, 28], [15, 29], [17, 29], [17, 30], [17, 31], [18, 31]]
+        for [x,y] in toSpawn:
+            self.grid[x,y] = 1
+    def stopperAnd10(self):
+        toSpawn = [[14, 68], [15, 68], [14, 69], [16, 69], [16, 70], [16, 71], [17, 71]]
+        for [x,y] in toSpawn:
+            self.grid[x,y] = 1
+    def stopperAnd00(self):
+        toSpawn = [[14, 68], [15, 68], [14, 69], [16, 69], [16, 70], [16, 71], [17, 71], [15, 28], [16, 28], [15, 29], [17, 29], [17, 30], [17, 31], [18, 31]]
+        for [x,y] in toSpawn:
+            self.grid[x,y] = 1
+    def spawnStopperAnd(self):
+        toSpawn = [[56, 55], [56, 56], [57, 56], [58, 55], [58, 54], [58, 53], [59, 53]]
+        for [x,y] in toSpawn:
+            self.grid[x,y] = 1
+
+    def spawnStopperNot(self):
+        toSpawn = [[14, 27], [15, 27], [14, 28], [16, 28], [16, 29], [16, 30], [17, 30]]
+        for [x,y] in toSpawn:
+            self.grid[x,y] = 1
+
+    def spawnOr01(self):
+        toSpawn = [[13, 66], [14, 66], [13, 67], [15, 67], [15, 68], [15, 69], [16, 69]]
+        for [x,y] in toSpawn:
+            self.grid[x,y] = 1
+
+    def spawnOr10(self):
+        toSpawn = [[13, 106], [14, 106], [13, 107], [15, 107], [15, 108], [15, 109], [16, 109]]
+        for [x,y] in toSpawn:
+            self.grid[x,y] = 1
+
+    def spawnOr00(self,r,c):
+        toSpawn = [[13, 66], [14, 66], [13, 67], [15, 67], [15, 68], [15, 69], [16, 69], [13, 106], [14, 106], [13, 107], [15, 107], [15, 108], [15, 109], [16, 109]]
+        for [x,y] in toSpawn:
+            self.grid[x,y] = 1
+        
+    
+    def spawnNOTGate(self,prop):
+        self.clearGrid()
+        self.spawnGlider(1,0)
+        self.spawnReverseGlider(3,40)
+        self.spawnEaterNot()
+        if not prop:
+            self.spawnStopperNot()
+    
+    def spawnANDGate(self,prop1,prop2):
+        self.clearGrid()
+        self.spawnGlider(1,0)
+        self.spawnGlider(0,40)
+        self.spawnReverseGlider(3,90)
+        self.spawnEaterOr()
+        if not prop1 and not prop2:
+            self.stopperAnd00()
+            self.spawnStopperAnd()
+        if not prop1 and prop2:
+            self.stopperAnd01()
+        if prop1 and not prop2:
+            self.stopperAnd10()
+            
+    
+            
+     
+    '''
     def makeGliderGun(self,x,y,orientation):
         # Requires 9 X 36 grid or 36 X 9 grid, depending on orientation
         if orientation == "NW": 
@@ -138,5 +236,5 @@ class gameoflifegrid():
     def makeNOTGateWithoutInput(self):
         self.clearGrid()
         self.makeGliderGun(40,0,"SW")
-        
+'''
         
